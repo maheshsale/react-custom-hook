98763c444b5c65115aba719131b237693a075658
Mahesh Sale <maheshsale333@gmail.com>
2023-11-12 14:09:51 +0530

    Custom hook created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import useRandomColor from './useRandomColor';
 
 function App() {
+  const {color, changeColor} = useRandomColor();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{width:"100vw", height:"100vh", backgroundColor:"#" + color}}>
+      <button className="color-changer" onClick={() => changeColor()}>Change Color</button>
     </div>
   );
 }
